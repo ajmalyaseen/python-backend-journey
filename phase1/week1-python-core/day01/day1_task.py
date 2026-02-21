a716d236c317593ed029cd5c87d97345540c074f
Yaa.si_ <83084169+ajmalyaseen@users.noreply.github.com>
2026-02-21 11:37:57 +0530

    feat: day1 string tasks completed

--- phase1/week1-python-core/day01/day1_task.py
+++ phase1/week1-python-core/day01/day1_task.py
@@ -32,3 +32,53 @@ print(f"Age: {age}")
 print(f"Weight: {weight} kg")
 print(f"Developer: {is_developer}")
 print(f"Next year age: {age + 1}")
+
+#string tasks 
+
+email = "ajmal.dev@gmail.com"
+# Extract and print:
+# 1. username → everything before @  (ajmal.dev)
+username=email.split('@')[0]
+print(username)
+# 2. domain   → everything after @   (gmail.com)
+domain=email.split("@")[1]
+print(domain)
+# 3. provider → just "gmail" (no .com)
+provider=domain.split(".")[0]
+print(provider)
+
+
+# A user submitted this data - clean it up
+username = "  AJMAl_YaSeeN  "
+email = "  AJMAL@GMAIL.COM  "
+
+# Print:
+# cleaned username → lowercase, stripped
+cleaned_username=username.strip().lower().replace("_","")
+print(f"Cleaned username: {cleaned_username}")
+# cleaned email    → lowercase, stripped
+cleaned_email=email.strip().lower()
+print(f"Cleaned email: {cleaned_email}") 
+# is username alphanumeric? → True or False (remove _ first using replace)
+print(cleaned_username.isalnum())
+ 
+
+
+
+password = "Ajmal@123"
+# Print:
+# Length of password
+length=len(password)
+# Has digit?     → True/False  (use any(c.isdigit() for c in password))
+has_digit = any(c.isdigit() for c in password)  # ✅
+# Has uppercase? → True/False  (use any(c.isupper() for c in password))
+has_upper = any(c.isupper() for c in password)
+# Is strong?     → True if length >= 8 AND has digit AND has uppercase
+is_strong=False
+if length>=8 and has_digit and has_upper:
+    is_strong=not is_strong
+
+print(f"Length: {length}")
+print(f"Has digit: {has_digit}")
+print(f"Has uppercase: {has_upper}")
+print(f"Is strong: {is_strong}")
